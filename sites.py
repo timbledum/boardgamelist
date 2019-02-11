494ad82f7b4b5c95bc56ea9665a2d082e4553e5c
timbledum <timbledum@gmail.com>
2019-02-11 21:35:00 +1300

    Add games by age and games by year.

--- sites.py
+++ sites.py
@@ -43,33 +43,6 @@ def weight(table):
 pages.append(Page("length.html", "Games by length", weight))
 
 
-def players(table):
-    columns_to_keep = ["Board Game Geek link", "Name", "Ranking", "Minutes to play"]
-
-    minimum_players = int(table.stats("Minimum players").min)
-    maximum_players = int(table.stats("Maximum players").max)
-
-    table_sorted = table.sort("Ranking")
-
-    def create_filter(player_count):
-        return lambda row: (row["Minimum players"] <= player_count) and (
-            row["Maximum players"] >= player_count
-        )
-
-    tables = []
-    for player_count in range(minimum_players, maximum_players + 1):
-        group_table = table_sorted.select(create_filter(player_count))
-        table_cut = group_table.cut(columns_to_keep)
-
-        header = f"Games with {player_count} player{'' if player_count == 1 else 's'}"
-        group = dict(header=header, table=table_cut)
-        tables.append(group)
-
-    return tables
-
-
-pages.append(Page("players.html", "Games by number of players", players, True))
-
 
 def categories(table):
     category_list = [
@@ -135,3 +108,65 @@ def categories(table):
 
 
 pages.append(Page("categories.html", "Games by category", categories, True))
+
+
+def players(table):
+    columns_to_keep = ["Board Game Geek link", "Name", "Ranking", "Minutes to play"]
+
+    minimum_players = int(table.stats("Minimum players").min)
+    maximum_players = int(table.stats("Maximum players").max)
+
+    table_sorted = table.sort("Ranking")
+
+    def create_filter(player_count):
+        return lambda row: (row["Minimum players"] <= player_count) and (
+            row["Maximum players"] >= player_count
+        )
+
+    tables = []
+    for player_count in range(minimum_players, maximum_players + 1):
+        group_table = table_sorted.select(create_filter(player_count))
+        table_cut = group_table.cut(columns_to_keep)
+
+        header = f"Games with {player_count} player{'' if player_count == 1 else 's'}"
+        group = dict(header=header, table=table_cut)
+        tables.append(group)
+
+    return tables
+
+
+pages.append(Page("players.html", "Games by number of players", players, True))
+
+
+
+
+def year(table):
+    columns_to_keep = ["Board Game Geek link", "Name", "Year published", "Ranking", "Minutes to play"]
+    table_sorted = table.sort("Minutes to play")
+    table_cut = table_sorted.cut(columns_to_keep)
+    return table_cut
+
+
+pages.append(Page("year.html", "Games by year", year))
+
+
+def minimum_age(table):
+    columns_to_keep = ["Board Game Geek link", "Name", "Ranking", "Minutes to play"]
+
+    ages = sorted(set(table["Minimum age"]))
+
+    table_sorted = table.sort("Ranking")
+
+    tables = []
+    for age in ages:
+        group_table = table_sorted.selecteq("Minimum age", age)
+        table_cut = group_table.cut(columns_to_keep)
+
+        header = f"Games with the minimum recommended age of {age}"
+        group = dict(header=header, table=table_cut)
+        tables.append(group)
+
+    return tables
+
+
+pages.append(Page("minimum_age.html", "Games by minimum age", minimum_age, True))
